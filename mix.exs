21d23d7ea7dfd366c65ba9bdcf18ce691435b990
Daniel Hippley <1974398+dhippley@users.noreply.github.com>
2025-07-12 10:35:45 -0400

    add styler

--- mix.exs
+++ mix.exs
@@ -18,7 +18,8 @@ defmodule MessagingServiceKit.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:styler, "~> 1.4", only: [:dev, :test], runtime: false},
     ]
   end
 end
